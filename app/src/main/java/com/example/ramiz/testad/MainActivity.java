2fc3d66cc79dd59c4fbb13ede946f3665542ea6b
Ramiz Khan <ramizkhan431@gmail.com>
2021-02-06 16:43:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ramiz/testad/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.ramiz.testad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+
+public class MainActivity extends AppCompatActivity {
+    private InterstitialAd mInterstitialAd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        prepareAD();
+        ScheduledExecutorService scheduledExecutorService=
+                Executors.newSingleThreadScheduledExecutor();
+        scheduledExecutorService.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (mInterstitialAd.isLoaded()){
+                            mInterstitialAd.show();
+                        }
+                        else {
+                            Log.d("TAG","not loded");
+
+                        }
+                        prepareAD();
+                    }
+                });
+            }
+        },10,10, TimeUnit.SECONDS);
+
+
+    }
+    public void prepareAD(){
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+    }
+}
